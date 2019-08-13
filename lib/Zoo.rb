15a2c0790341f8940b14b82247342ad506f75dcb
Josh Nho <joshnho@gmail.com>
2019-08-13 16:10:05 -0400

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,45 @@
 class Zoo
+    attr_reader :location, :name
 
+    @@all = []
+
+    def initialize(name, location)
+        @name = name
+        @location = location
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end
+
+    def animals
+        Animal.all.select do |animal|
+            animal.zoo == self
+        end
+    end
+
+    def animal_species
+        self.animals.map do |animals|
+            animals.species
+        end.uniq
+    end
+
+    def find_by_species(species)
+        self.animals.select do |animal|
+            animal.species == species
+        end
+    end
+
+    def animal_nicknames
+        self.animals.map do |animals|
+            animals.nickname
+        end
+    end
+
+    def self.find_by_location(location_find)
+        self.all.select do |zoo|
+            zoo.location == location_find
+        end
+    end
 end
--- run.rb
+++ run.rb
@@ -5,6 +5,17 @@ require 'pry'
 
 #Test your code here
 
+cat1 = Animal.new("cat", 20, "Nina")
+cat2 = Animal.new("cat", 15, "Kitty")
+dog1 = Animal.new("dog", 50, "Spike")
+turtle1 = Animal.new("turtle", 100, "Yoshi")
 
+zoo1 = Zoo.new("Bronx Zoo", "Bronx, NY")
+zoo2 = Zoo.new("New Zoo", "Long Island, NY")
+
+cat1.zoo = zoo1
+cat2.zoo = zoo1
+dog1.zoo = zoo2
+turtle1.zoo = zoo1
 binding.pry
-puts "done"
+0
