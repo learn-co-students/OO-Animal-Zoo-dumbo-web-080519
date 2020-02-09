07e53dc8a240a3de908abe3747b2909705e408f0
Zermina Ejaz <zerminaejaz@gmail.com>
2020-02-09 11:37:40 -0500

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,17 @@
 class Zoo
 
+    attr_accessor :name, :location
+
+    def all
+        @@all
+    end
+
+    def initialize(name, location)
+        @location = location
+        @name = name
+        @@all << self
+    end
+
+
+
 end
